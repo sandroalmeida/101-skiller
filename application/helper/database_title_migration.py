d4daa0b2be4135bb56f55174641262b2f6898052
Sandro Almeida <sandrodotbuild@gmail.com>
2023-06-22 21:02:44 -0700

    excluding official title

--- application/helper/database_title_migration.py
+++ application/helper/database_title_migration.py
@@ -9,7 +9,6 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 resources_path = os.path.join(current_dir, '..', 'resources')
 sys.path.append(resources_path)
 
-from v2.official_title import official_title
 from v2.official_title_regex import official_title_regex
 from v2.title_patterns_dictionary import title_patterns
 
@@ -23,28 +22,11 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 # Title
-for key in official_title.keys():
-    title_description = key
-    sql = "INSERT IGNORE INTO title (title_id, description) VALUES (UUID(), %s)"
-    val = (title_description,)
-    mycursor.execute(sql, val)
-
 for key in official_title_regex.keys():
     title_description = key
     sql = "INSERT IGNORE INTO title (title_id, description) VALUES (UUID(), %s)"
     val = (title_description,)
     mycursor.execute(sql, val)
-
-# Title String Dictionary
-for key, aliases in official_title.items():
-    title_description = key
-    mycursor.execute("SELECT title_id FROM title WHERE description = %s", (title_description,))
-    title_id = mycursor.fetchone()[0]
-
-    for alias in aliases:
-        sql = "INSERT INTO title_string_dictionary (alias, title_id) VALUES (%s, %s)"
-        val = (alias, title_id)
-        mycursor.execute(sql, val)
 
 # Title Regex Dictionary
 for key, regex_list in official_title_regex.items():
@@ -57,15 +39,20 @@ for key, regex_list in official_title_regex.items():
         val = (regex, title_id)
         mycursor.execute(sql, val)
 
-# # Title Pattern
+# Title Pattern
 for key, pattern in title_patterns.items():
     title_description = key
     mycursor.execute("SELECT title_id FROM title WHERE description = %s", (title_description,))
     title_id = mycursor.fetchone()[0]
 
     for pattern in pattern:
-        sql = "INSERT INTO title_pattern (name, pattern, title_id) VALUES (%s, %s, %s)"
-        val = (pattern[0], pattern[1], title_id)
-        mycursor.execute(sql, val)
+        mycursor.execute("SELECT name FROM title_pattern WHERE pattern = %s", (pattern[1],))
+        pattern_existent = mycursor.fetchone()
+        if pattern_existent:
+            print("Pattern already exists." + pattern[1])
+        else:
+            sql = "INSERT INTO title_pattern (name, pattern, title_id) VALUES (%s, %s, %s)"
+            val = (pattern[0], pattern[1], title_id)
+            mycursor.execute(sql, val)
 
 mydb.commit()
